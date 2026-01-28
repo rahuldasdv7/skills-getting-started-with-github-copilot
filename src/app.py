0f7ce861e830c5258718584f017e7af8760e3fbf
rahuldasdv7 <146253083+rahuldasdv7@users.noreply.github.com>
2026-01-28 20:17:11 +0000

    add additional activity

--- src/app.py
+++ src/app.py
@@ -39,6 +39,12 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
+    },
+    "Art Club": {
+        "description": "Explore various art techniques and create artworks",
+        "schedule": "Wednesdays, 3:30 PM - 5:00 PM",
+        "max_participants": 15,
+        "participants": ["lucas@mergington.edu", "ava@mergington.edu"]
     }
 }
 

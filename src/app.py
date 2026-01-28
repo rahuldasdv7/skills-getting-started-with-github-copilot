9ae49ba3d1a05b41c0af4d5845ef919bc5943198
rahuldasdv7 <146253083+rahuldasdv7@users.noreply.github.com>
2026-01-28 20:13:51 +0000

    .Prevent duplicate signups

--- src/app.py
+++ src/app.py
@@ -20,6 +20,7 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
           "static")), name="static")
 
 # In-memory activity database
+ m-memory activity database
 activities = {
     "Chess Club": {
         "description": "Learn strategies and compete in chess tournaments",
@@ -61,7 +62,8 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Get the specific activity
     activity = activities[activity_name]
-
+    # Validate Student not already signed up
+    if email in activity["participants"]:
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
